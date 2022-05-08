7ea168a637f635605838e9b4a25dffd2e9d68de7
Vicky-18-2 <vicky.od.00@gmail.com>
2022-05-08 14:43:42 +0300

    initial commit

--- src/main/java/service/cinema/Cinema.java
+++ src/main/java/service/cinema/Cinema.java
@@ -1,4 +1,12 @@
 package service.cinema;
 
 public class Cinema {
+
+    protected int idCinema;
+
+    protected String cinemaName;
+
+    protected String cinemaCity;
+
+    protected String cinemaStreet;
 }
